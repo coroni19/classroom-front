cc0c344c1dc83fbeb95919a437d9c0613c4b4d71
coroni19 <roni@combina.co.il>
2025-07-30 18:37:41 +0300

    theme

--- src/contexts/Theme.context.tsx
+++ src/contexts/Theme.context.tsx
@@ -1,8 +1,8 @@
 import { themes } from "../../themes";
-import { type FC, type ReactNode } from "react";
-import { createContext, useContext } from "react";
+import { useRef, type FC, type ReactNode } from "react";
 import { LOCAL_STORAGE_THEME_KEY } from "../constants/keys.const";
 import usePersistantState from "../hooks/use-persistant-state.hook";
+import { createContext, useContext, useInsertionEffect } from "react";
 
 type TTheme = {
   color: string;
@@ -25,10 +25,23 @@ const ThemeContext = createContext<IThemeContext>({
 export const useThemeContext = () => useContext<IThemeContext>(ThemeContext);
 
 const DarkThemeProvider: FC<IProviderProps> = ({ children }) => {
+  const meta = useRef<HTMLMetaElement | null>(null);
+
   const [theme, setTheme] = usePersistantState<TTheme>(
     LOCAL_STORAGE_THEME_KEY,
     themes.dark
   );
+
+  useInsertionEffect(() => {
+    if (!meta.current) {
+      meta.current = document.createElement("meta");
+    }
+
+    meta.current.name = "theme-color";
+    meta.current.content = theme.color;
+
+    document.head.append(meta.current);
+  }, [theme.color]);
 
   const toggleTheme = () => {
     setTheme((prev) => (prev === themes.dark ? themes.light : themes.dark));
