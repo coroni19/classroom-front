f889e1271655cfd15e98bdaf4d56e4532ad16127
coroni19 <roni@combina.co.il>
2025-07-31 09:20:57 +0300

    meta head theme in toggle theme

--- src/contexts/Theme.context.tsx
+++ src/contexts/Theme.context.tsx
@@ -2,7 +2,7 @@ import { themes } from "../../themes";
 import { type FC, type ReactNode } from "react";
 import { LOCAL_STORAGE_THEME_KEY } from "../constants/keys.const";
 import usePersistantState from "../hooks/use-persistant-state.hook";
-import { createContext, useContext, useInsertionEffect } from "react";
+import { createContext, useContext } from "react";
 
 type TTheme = {
   color: string;
@@ -25,29 +25,22 @@ const ThemeContext = createContext<IThemeContext>({
 export const useThemeContext = () => useContext<IThemeContext>(ThemeContext);
 
 const DarkThemeProvider: FC<IProviderProps> = ({ children }) => {
-  // const meta = useRef<HTMLMetaElement | null>(null);
-
   const [theme, setTheme] = usePersistantState<TTheme>(
     LOCAL_STORAGE_THEME_KEY,
     themes.dark
   );
 
-  useInsertionEffect(() => {
-    // if (!meta.current) {
-    //   meta.current = document.createElement("meta");
-    // }
-
-    // meta.current.name = "theme-color";
-    // meta.current.content = theme.color;
-    // meta.current.id = "theme"
-
-    document.querySelector('meta[name="theme-color"]')?.setAttribute("content", theme.color)
-    // document.head.remove
-    // document.head.append(meta.current);
-  }, [theme.color]);
+  // useInsertionEffect(() => {
+  //   document
+  //     .querySelector('meta[name="theme-color"]')
+  //     ?.setAttribute("content", theme.color);
+  // }, [theme.color]);
 
   const toggleTheme = () => {
     setTheme((prev) => (prev === themes.dark ? themes.light : themes.dark));
+    document
+      .querySelector('meta[name="theme-color"]')
+      ?.setAttribute("content", theme.color);
   };
 
   return (
